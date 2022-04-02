bd6c0ffd5a92efeda9feb4b34a0b779e2abd5320
unknown <eliasjrcosta886@gmail.com>
2022-04-02 11:35:54 -0300

    formatando as strings e validadando inputs

--- my-projects/ctof.js
+++ my-projects/ctof.js
@@ -6,19 +6,25 @@ function calcular(){
     let resp = document.getElementById('conteudomodal')
     let eq = document.getElementById('equacao')
 
-    if(c1.value == "" && c2.value ==""){
+    if(c1.value == "" && c2.value == ""){
         window.alert("[ERRO] PARA COMEÇAR, INFORME UM VALOR !!!")
 
-    } else if(c1.value == 0 && c2.value == 0){
-        window.alert("[ERRO] PARA COMEÇAR, INFORME UM VALOR DIFERENTE DE ZERO !!!")
-    } else{
+    } else if(c1.value != '' && c2.value == ''){
         let modal = document.querySelector('.modal')
         modal.style.display = "block"
 
         let res = c1.value * 9/5 + 32
-        resp.innerHTML= `${c1.value}C é equivalente a ${res}F`
+        resp.innerHTML= `${c1.value}°C é equivalente a ${res.toFixed(2)}°F`
         eq.innerHTML= `${c1.value} x 9/5 + 32 `
+    } else if(c2.value != '' && c1.value == ''){
+        let modal = document.querySelector('.modal')
+        modal.style.display = "block"
 
+        let res = (c2.value - 32) * 5/9
+        resp.innerHTML= `${c2.value}°F é equivalente a ${res.toFixed(2)}°C`
+        eq.innerHTML= `${c2.value} - 32 x 5/9 `
+    } else {
+        window.alert('[ERRO] PREENHA APENAS UM CAMPO POR VEZ !!! ')
     }
 }
 
